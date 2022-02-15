f203f1edc95e83c53ee52fd84d3d2e57d1caf54c
wahyu rizky ramadhan <wahyurizkyramadhan007@gmail.com>
2022-02-15 10:53:28 +0700

    add header component

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white border-0">
+        <img
+          src="/assets/images.jpg"
+          class="card-img"
+          alt="Background"
+          height="550px"
+        />
+        <div class="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 class="card-title display-3 fw-bolder mb-0">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
