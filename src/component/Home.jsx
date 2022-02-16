3a4213146248bca06c4ae3956f1b00ff59c4a68c
wahyu rizky ramadhan <wahyurizkyramadhan007@gmail.com>
2022-02-16 11:15:10 +0700

    update product and setfilter

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import Products from "./Products";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
+      <div className="card bg-dark text-white border-0">
         <img
           src="/assets/images.jpg"
-          class="card-img"
+          className="card-img"
           alt="Background"
           height="550px"
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
-            <h5 class="card-title display-3 fw-bolder mb-0">
+            <h5 className="card-title display-3 fw-bolder mb-0">
               NEW SEASON ARRIVALS
             </h5>
-            <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
           </div>
         </div>
       </div>
+      <Products />
     </div>
   );
 };
